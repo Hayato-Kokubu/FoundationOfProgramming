4bd33e77415d3618912aebd51801198ef94eb81e
hayatokokubu <kokubu.hayato@gmail.com>
2019-06-17 08:40:38 +0900

    q5.2 step3(true, false both case in)

--- ch5/jikan.ml
+++ ch5/jikan.ml
@@ -1,8 +1,8 @@
 (* 時間 h を受け取ったら、"午前" か "午後" かを返す関数 jikan *)
-(* jikan: int -> String *)
+(* jikan: int -> string *)
 
-let jikan h = if( h < 12)  then "hoge"
-                           else "fuga"
+let jikan h = if( h < 12)  then "午前"
+                           else "午後"
 
 (*テスト*)
 let test1 = ( jikan 1 = "午前")
