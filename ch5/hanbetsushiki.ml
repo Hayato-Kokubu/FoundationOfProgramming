086f36daad121909446a2034dff45c182c48991f
hayatokokubu <kokubu.hayato@gmail.com>
2019-06-17 11:37:37 +0900

    q4 step2

--- ch5/hanbetsushiki.ml
+++ ch5/hanbetsushiki.ml
@@ -5,7 +5,7 @@ let abs_value x =
 
 (* 2次方程式 a * x^2 + b * x + c の 係数 a,b,c を受け取り、判別式を返す関数 hanbetsushiki *)
 (* hanbetsushiki: float -> float -> float -> float *)
-let hanbetsushiki a b c = 0.0
+let hanbetsushiki a b c = b ** 2.0 -. 4.0 *. a *. c
 
 (* テスト *)
 (* let test1 = hanbetsushiki 1.0 0.0 (-1.0) = 4.0 *)
