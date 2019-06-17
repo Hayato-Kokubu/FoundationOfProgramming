058809caec4a37b3d1b953256ebd4b746cfee630
hayatokokubu <kokubu.hayato@gmail.com>
2019-06-17 12:09:16 +0900

    q7 step2

--- ch5/taikei.ml
+++ ch5/taikei.ml
@@ -5,8 +5,10 @@ let bmi m kg = kg /. m ** 2.0
 (* 身長 m, 体重 kg を受け取り、これらによって求まるbmi値に応じて体型を返す関数 taikei *)
 (* taikei: float -> float -> string *)
 
-let taikei m kg = ""
-
+let taikei m kg =  if(bmi m kg < 18.5) then "痩せ"
+              else if(bmi m kg < 25.0) then "標準"
+              else if(bmi m kg < 30.0) then "肥満"
+              else  "高度肥満"
 
 (*テスト*)
 let test1 = taikei 1.5 40.0 = "痩せ"
