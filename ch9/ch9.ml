af0beb75d67edabc5eebb209d5a5e78a2b18c907
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-10 06:35:13 +0900

    リストの再帰

--- ch9/ch9.ml
+++ ch9/ch9.ml
@@ -6,5 +6,31 @@ let func l = match l with
     [] -> 0
 |   first :: rest -> first
 
+
 let res0 = func empty
 let res1 = func list
+
+(*
+ int list は
+ - []               空リスト または
+ - first :: rest    最初の要素がfirst , 残りのリストがrest
+ という形
+ *)
+
+(* 目的: 受け取ったリスト lst に  0 が含まれているかどうかを調べる *)
+(* contain_zero: int list -> bool *)
+
+let rec contain_zero lst = match lst with
+    [] -> false 
+|   first :: rest ->  if first = 0 then true 
+                                   else contain_zero rest
+
+
+(* test *)
+let test1 = contain_zero [] = false
+let test2 = contain_zero [1; 2] = false
+let test3 = contain_zero [0; 2] = true
+let test4 = contain_zero [1;2;3;0;5;6;7] = true
+let test5 = contain_zero [1;2;3;4;5;6;7] = false
+
+
