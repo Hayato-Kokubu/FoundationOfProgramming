40b78d589cc879dd050b3d1e7e81c11b0d8910a9
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-10 07:12:52 +0900

    impl even

--- ch9/q9_5.ml
+++ ch9/q9_5.ml
@@ -10,7 +10,9 @@
 
 let rec even lst = match lst with 
     [] -> []
-|   first :: rest -> [] (* even rest *)
+|   first :: rest -> 
+        if first mod 2 = 0 then first :: even rest
+                           else even rest
 
 (* test *)
 let evenTest1 = even []    = []
@@ -20,3 +22,14 @@ let evenTest4 = even [1;2] = [2]
 let evenTest5 = even [2;1] = [2]
 let evenTest6 = even [1;3] = []
 let evenTest6 = even [2;4] = [2;4]
+
+
+(* 
+  even [0;1;2;3]
+= even 0 :: even [1;2;3]
+= even 0 ::(even [2;3]) 
+= even 0 ::(2 :: even [3])
+= even 0 ::(2 :: even [])
+= even 0 ::(2 :: [])
+= [0;2]
+*)
